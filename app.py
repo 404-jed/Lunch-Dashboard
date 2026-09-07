5d2035e367dca6fe4cbb85f707f0c004e126d7fe
Jedidiah Armah <ja60135@email.vccs.edu>
2026-09-07 00:01:05 -0400

    db creation

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+from flask_login import UserMixin
+
 
 app = Flask(__name__)
 
+def configure():
+    load_dotenv()
+
+# general lunch menu database
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 @app.route('/')
 def home():
