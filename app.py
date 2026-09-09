ea5e273b444ed5184e7bf47c203d153b227bde30
Jedidiah Armah <ja60135@email.vccs.edu>
2026-09-09 19:24:20 -0400

    initial authorization commit

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+
+
 from flask import Flask, render_template, url_for, redirect, flash, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv
 
-from flask_login import UserMixin, LoginManager, login_required, logout_user, login_user
+from flask_login import UserMixin, LoginManager, login_required, logout_user, login_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Email, Length,ValidationError
@@ -28,6 +30,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(254), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(), nullable=False)
 
 
 
@@ -51,7 +54,7 @@ class RegistrationForm(FlaskForm):
 
     def validate_email(self, email) -> None:
         existing_user = db.session.execute(db.select(User).filter_by(email=email.data)).scalar_one_or_none()
-        domain = email.data.split("@")[-1]
+        domain = email.data.split('@')[-1]
         if existing_user:
             raise ValidationError("Email already registered, Login or choose a different one")
 
@@ -88,7 +91,10 @@ def register():
     # On form submission our hashed password and unique user will be added and commited into the database
     if register_form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(password=register_form.password.data)
-        unique_user = User(email=register_form.email.data, password=hashed_password)
+        user_role = "worker" if register_form.email.data.split('@')[-1] == "coderva.org" else "student"
+        unique_user = User(email=register_form.email.data, password=hashed_password, role=user_role)
+
+
         db.session.add(unique_user)
         db.session.commit()
         return redirect(url_for("login"))
@@ -108,6 +114,8 @@ def login():
     login_form = LoginForm()
 
     user = db.session.execute(db.select(User).where(User.email == login_form.email.data)).scalar_one_or_none()
+
+
     if login_form.validate_on_submit():
         login_user(user)
         flash("Logged in successfully")
